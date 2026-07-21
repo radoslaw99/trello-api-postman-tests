8a12eeb8b376d037dfd64d56f3df09eb3bf7b4e3
radoslawcyboron <147160180+radoslaw99@users.noreply.github.com>
2026-07-21 20:36:35 +0200

    Update run-newman-from-postman-api.bat
    Add Newman execution script

--- scripts/run-newman-from-postman-api.bat
+++ scripts/run-newman-from-postman-api.bat
@@ -1,1 +1,7 @@
+@echo off
 
+newman run "https://api.getpostman.com/collections/COLLECTION_UID" ^
+-e "https://api.getpostman.com/environments/ENVIRONMENT_UID" ^
+--postman-api-key "%POSTMAN_API_KEY%" ^
+--env-var "key=%TRELLO_KEY%" ^
+--env-var "token=%TRELLO_TOKEN%"
